1a8942b2e8c7e5ebf9ecf0d6ac3a8bc8ecdb196b
梁祐荣_金腾科技 <liangyourong@cjis.wan>
2025-12-03 23:38:14 +0800

    deploy2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/csyprotfolio', 
   plugins: [vue()],
+  base: '/csyprotfolio/', // 必须与仓库名一致
+  build: {
+    outDir: 'dist',       // 确保输出到 dist 目录
+    assetsDir: 'assets',  // 静态资源目录
+    emptyOutDir: true     // 构建前清空目录
+  }
 })
